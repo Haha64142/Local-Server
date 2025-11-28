a067f4fce18047e48451b6552b3a58c87377665a
Haha64142 <paul.w64142@gmail.com>
2025-11-28 12:36:38 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import path from "path";
+import readline from "readline";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = process.env.PORT || 80;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let data;
+let filename;
+
+app.use(express.raw({ type: "*/*" }));
+
+app.post("/data", async (req, res) => {
+  data = req.body;
+  filename = req.headers["x-filename"] || "response.bin";
+  res.sendStatus(200);
+});
+
+app.get("/data", async (req, res) => {
+  if (!data) {
+    return res.status(404).send("No data available");
+  }
+
+  res.setHeader("Content-Type", "application/octet-stream");
+  res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
+  res.send(data);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  console.log("\nPress Enter to stop the server");
+  waitForInput();
+});
+
+function waitForInput() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on("line", () => {
+    console.log("Stopping server...");
+    server.close(() => {
+      console.log("Server stopped");
+      process.exit(0);
+    });
+  });
+}
